6ebe55f43e364094ff190c2b8a33a36247272540
Jonatan Navarro <navarrojonatan.e@gmail.com>
2023-12-07 16:25:53 -0300

    Configuracion inicial: Release 1.0 Seteo de mapa y splash inicial

---
+++ app/src/main/java/com/Home/HomeActivity.kt
@@ -0,0 +1,30 @@
+package com.Home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.blankbusapp.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+    }
+}
